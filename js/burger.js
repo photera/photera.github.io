7b38734219273152099f452f269558c863ea86b2
blackthought <antonio.a.james@gmail.com>
2015-06-16 14:59:01 -0400

    changed speed of fade

--- js/burger.js
+++ js/burger.js
@@ -3,13 +3,13 @@
 $(document).ready(function () {
     $('.burger-contain').click(function () {
         $('.burger-nav').toggleClass('open');
-        $('.burger-nav').slideToggle(600);
+        $('.burger-nav').fadeToggle(300);
         $(this).toggleClass('open');
         $('.burger-brand').toggleClass('open');
     });
     $('.burger-nav li').click(function(){
         $('.burger-nav').toggleClass('open');
-        $('.burger-nav').fadeToggle(600);
+        $('.burger-nav').fadeToggle(300);
         $(this).toggleClass('open');
         $('.burger-brand').toggleClass('open');
     });
