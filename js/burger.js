28a217b6d85ffee8047d071adf6d5ae5b8c9d02d
blackthought <antonio.a.james@gmail.com>
2015-06-15 20:36:59 -0400

    BRUGER

---
+++ js/burger.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$(document).ready(function () {
+    $('.burger-contain').click(function () {
+        $('.burger-nav').toggleClass('open');
+        $('.burger-nav').fadeToggle(600);
+        $(this).toggleClass('open');
+        $('.burger-brand').toggleClass('open');
+    });
+    $('.burger-nav li').on('click', function(){
+        $(".burger-nav").hide();
+        $("#burger-contain").removeClass("open");
+    });
+});
