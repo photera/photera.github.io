87b7952f6a148e35d9d4442853a1b0080aafe9f9
blackthought <antonio.a.james@gmail.com>
2015-06-26 22:02:05 -0400

    yeah right

---
+++ js/main.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function($){
+	//open the lateral panel
+	$('.cd-btn').on('click', function(event){
+		event.preventDefault();
+		$('.cd-panel').addClass('is-visible');
+		$("body").css("overflow", "hidden");
+	});
+	//close the lateral panel
+	$('.cd-panel').on('click', function(event){
+		if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) { 
+			$('.cd-panel').removeClass('is-visible');
+			$("body").css("overflow", "initial");
+			event.preventDefault();
+		}
+	});
+});
+
+
+jQuery(document).ready(function($){
+	//open the lateral panel
+	$('.cd-btn-3').on('click', function(event){
+		event.preventDefault();
+		$('.cd-panel-3').addClass('is-visible');
+		$("body").css("overflow", "hidden");
+	});
+	//close the lateral panel
+	$('.cd-panel-3').on('click', function(event){
+		if( $(event.target).is('.cd-panel-3') || $(event.target).is('.cd-panel-close') ) { 
+			$('.cd-panel-3').removeClass('is-visible');
+			$("body").css("overflow", "initial");
+			event.preventDefault();
+		}
+	});
+});
