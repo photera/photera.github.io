49b038ad9123b0a3b25162f017987311bdd0a0d8
blackthought <antonio.a.james@gmail.com>
2015-08-17 14:46:51 -0500

    restored proj contaitn

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ jQuery(document).ready(function($){
 	});
 
 	//close project
-	$('.projects-container .projects-nav .cd-close').on('click', function(){
+	$('.projects-container .cd-close').on('click', function(){
 		toggleProject($('.is-full-width'), $('.projects-container'), false);
 	});
 
